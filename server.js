4eec71755d74531065b64ade24e97dad8124254f
Evan Turner <evturn@gmail.com>
2015-04-29 15:27:59 -0400

    request added with route to /search

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app     = express();
+var request = require('request');
 
 
 app.use(express.static(__dirname + '/public'));
@@ -7,6 +8,18 @@ app.set('view engine', 'html');
 
 app.get('/', function(req, res){
   res.render('index');
+});
+
+app.get('/search', function(req, res) {
+	
+
+	var request = require('request');
+	request('http://api.yelp.com/v2/search/?location=New%20York,%20NY', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(body) // Show the HTML for the Google homepage. 
+	  }
+	});
+
 });
 
 app.listen(8000, function(){
