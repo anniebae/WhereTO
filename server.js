60731d60fdeb0961536b77efc99ecd2a808c6b1b
anniebae <bae.annie@gmail.com>
2015-04-29 17:03:59 -0400

    parsing through body of request object to find params

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var app     = express();
-var request = require('request');
-var yelp = require("yelp").createClient({
+var express          = require('express');
+var app              = express();
+var request          = require('request');
+var bodyParser       = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var yelp             = require("yelp").createClient({
   consumer_key: process.env.YELP_CONSUMER_KEY, 
   consumer_secret: process.env.YELP_CONSUMER_SECRET,
   token: process.env.YELP_TOKEN,
@@ -15,13 +17,22 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
-app.get('/api', function(req, res) {
-  yelp.business("yelp-san-francisco", function(error, data) {
+
+app.post('/api', urlencodedParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  var request = req.body;
+  var cityInput = request.city;
+  var location = cityInput.split(' ').join('-');
+  var city = 'yelp-' + location;
+  yelp.business(city, function(error, data) {
     console.log(error);
     console.log(data);
     res.json(data);
+    
+    console.log(data.name);
   });
 });
+
 
 app.listen(8000, function(){
     console.log("WhereTO running");
