8e36410c73dd320baa42cdb10b2f281e4d6192d5
anniebae <bae.annie@gmail.com>
2015-04-29 15:38:11 -0400

    yelp api keys required

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app     = express();
 var request = require('request');
+var yelp = require("yelp").createClient({
+  consumer_key: process.env.YELP_CONSUMER_KEY, 
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
+  token: process.env.YELP_TOKEN,
+  token_secret: process.env.YELP_TOKEN_SECRET
+});
 
 
 app.use(express.static(__dirname + '/public'));
