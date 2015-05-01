c33c026440b8aa73e13bb23f866c23da354106bc
anniebae <bae.annie@gmail.com>
2015-05-01 16:23:28 -0400

    navbar toggles when clicking on menu item

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,15 @@
-$(function(){
-  
-});
-
 new AppView();
+
+$(function(){
+  $('#btn-submit').on('click', function(e){
+    e.preventDefault();
+    console.log("submit clicked");
+  });  
+
+
+
+  $('.nav a').on('click', function(){
+      $(".navbar-toggle").click() //bootstrap 3.x by Richard
+  });
+
+});
