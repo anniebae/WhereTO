2179a478cd15e61c40a0c2270b2b377567ff208c
Evan Turner <evturn@gmail.com>
2015-05-01 16:57:53 -0400

    keypress event added for location

--- public/js/main.js
+++ public/js/main.js
@@ -2,28 +2,41 @@ new AppView();
 
 $(function(){
 
- 
-	$('#btn-brunch').on('click', function(e) {
-		e.preventDefault();
-		console.log('me been clicked');
-		var location = $('#brunchInput').val();
-		console.log(location);
-		$.ajax({
-			url: '/api/search',
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				location: location
-			},
-			success: function(data){
-				console.log(data);
-			}
-		});
-	});
-
   $('.nav a').on('click', function(){
       $(".navbar-toggle").click() //bootstrap 3.x by Richard
   });
 
 
+
+ 	$('#brunchInput').on('keypress', function(e) {
+ 		if (e.which === 13) {
+ 			sendData();
+ 		}
+ 	});
+
+	$('#btn-brunch').on('click', function(e) {
+		e.preventDefault();
+		sendData();
+	});
+
+
+
 });
+
+
+function sendData() {
+	console.log('me been clicked');
+	var location = $('#brunchInput').val();
+	console.log(location);
+	$.ajax({
+		url: '/api/search',
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			location: location
+		},
+		success: function(data){
+			console.log(data);
+		}
+	});
+}
