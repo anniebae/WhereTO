e353e7f92ac4e6c8faa41676132a7e2be4616fce
anniebae <bae.annie@gmail.com>
2015-05-08 17:16:53 -0400

    removing dummy placeholders

--- public/js/main.js
+++ public/js/main.js
@@ -2,14 +2,11 @@ var router = new Router();
 Backbone.history.start();
 
 $(function(){
-
   $('.nav a').on('click', function(){
-      $(".navbar-toggle").click() //bootstrap 3.x by Richard
+      $(".navbar-toggle").click()
   });
   $('[data-toggle="tooltip"]').tooltip()
-
 });
-
 
 var ref = new Firebase("https://where-to.firebaseio.com");
 ref.onAuth(function(authData) {
