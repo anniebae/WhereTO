31e5ea450f772bf1c307448ee1d91b213e5a39f6
Evan Turner <evturn@gmail.com>
2015-05-17 02:33:55 -0400

    comments added to server

--- server.js
+++ server.js
@@ -1,35 +1,33 @@
 var express          = require('express');
 var app              = express();
-var Firebase         = require('firebase');
 var handlebars       = require('express-handlebars');
 var apiRouter        = require('./routes/api-router');
 var appRouter        = require('./routes/app-router');
-var root = __dirname + '/public';
 
+// Config
+var root = __dirname + '/public';
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+app.engine('hbs', hbs.engine);
+app.use(express.static(root));
+
+// Handlebars
 var viewDirectories = [
   'views/welcome',
   'views/query',
   'views/dependencies',
   'views/partials'
 ];
-
 var hbs = handlebars.create({
     defaultLayout:'main',
     extname: '.hbs',
     partialsDir: viewDirectories
-
   });
 
-app.set('view engine', 'hbs');
-app.set('views', 'views');
-app.engine('hbs', hbs.engine);
-app.use(express.static(root));
-
-
+// Routes
 app.get('/', appRouter);
 app.get('/dashboard', appRouter);
 app.post('/api/search', apiRouter);
-
 
 app.listen(8000, function(){
     console.log("WhereTO running");
