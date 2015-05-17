1d32edded536a36671f3f72a60a26c4e2bd45b84
Evan Turner <evturn@gmail.com>
2015-05-17 04:56:22 -0400

    user schema written

--- models/user.js
+++ models/user.js
@@ -1,1 +1,21 @@
-var bcrypt = require('bcrypt');
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt');
+
+var userSchema = mongoose.Schema({
+  local            : {
+  		name				 : String,
+      email        : String,
+      username		 : String,
+      password     : String
+  }
+ });
+
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
