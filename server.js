ef3ce1adf8e4ffdb89e5efec5d1bc4b9c636b6b1
Evan Turner <evturn@gmail.com>
2015-05-17 10:17:35 -0400

    comments made on server file

--- server.js
+++ server.js
@@ -8,21 +8,17 @@ var mongoose      = require('mongoose');
 var passport      = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-
-var routes = require('./routes/router');
-var apiRouter = require('./routes/api-router');
-
 var app = express();
 
-
-// Express Config
 var root = __dirname + '/public';
 
+// Handlebars
 var hbs = require('./config/handlebars');
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 app.engine('hbs', hbs.engine);
 
+// Express Configuration
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,20 +28,30 @@ app.use(require('express-session')({
   resave: false,
   saveUninitialized: false
 }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.static(root));
 
+// Authentication Initialization
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes
+var routes = require('./routes/router');
+var apiRouter = require('./routes/api-router');
 app.use('/', routes);
 app.use('/api', apiRouter);
 
+// Models
 var User = require('./models/user');
+
+// Authentication Configuration
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-var database       = require('./config/database')(mongoose);
+// Database
+var database = require('./config/database')(mongoose);
+
+// Authentication Controller
 var authentication = require('./config/authentication')(passport);
 
 
