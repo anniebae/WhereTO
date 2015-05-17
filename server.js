4c3d60349806435faf277c02babc432e6dec1c85
Evan Turner <evturn@gmail.com>
2015-05-17 00:49:25 -0400

    handlebars brought in to replace ejs

--- server.js
+++ server.js
@@ -2,40 +2,39 @@ var express          = require('express');
 var app              = express();
 var request          = require('request');
 var bodyParser       = require('body-parser');
-var ejs              = require('ejs');
 var Firebase         = require('firebase');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
-var yelp             = require("yelp").createClient({
+var hbs = require('express-handlebars').create({
+    defaultLayout:'main',
+    extname: '.hbs'
+  });
+
+var root = __dirname + '/public';
+
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+app.engine('hbs', hbs.engine);
+app.use(express.static(root));
+
+
+app.get('/', function(req, res){
+  res.render('welcome/index');
+});
+
+app.get('/dashboard', function(req, res) {
+  res.render('query/index', {layout: 'main'});
+});
+
+
+
+
+
+
+var yelp = require("yelp").createClient({
   consumer_key: process.env.YELP_CONSUMER_KEY, 
   consumer_secret: process.env.YELP_CONSUMER_SECRET,
   token: process.env.YELP_TOKEN,
   token_secret: process.env.YELP_TOKEN_SECRET
-});
-var root = __dirname + '/public';
-var ref = new Firebase('https://where-to.firebaseio.com');
-
-app.use(express.static(root));
-app.set('view engine', 'ejs');
-app.set('views', root + '/views');
-
-app.get('/', function(req, res){
-  res.render('layouts/welcome');
-});
-
-app.post('/api/authenticate', urlencodedParser, function(req, res) {
-  if (!req.body) return res.sendStatus(400)
-    var request = req.body;
-    var token = request.user('token');
-    console.log(token);
-  if (token === "")
-    res.redirect('/');
-  else {
-    res.redirect('/dashboard');
-  }
-});
-
-app.get('/dashboard', function(req, res) {
-  res.render('layouts/application');
 });
 
 app.post('/api/search', urlencodedParser, function(req, res) {
