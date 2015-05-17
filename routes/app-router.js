781ff06ea87c81d8feac00175b594a78c430d15d
Evan Turner <evturn@gmail.com>
2015-05-17 04:36:17 -0400

    bring in passport and bcrypt for user auth

--- routes/app-router.js
+++ routes/app-router.js
@@ -1,12 +1,33 @@
-var express = require('express');
+var express 		  = require('express');
+var bodyParser    = require('body-parser');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+var passport      = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var flash         = require('connect-flash');
+var bcrypt        = require('bcrypt');
 var app = express.Router();
 
-app.get('/', function(req, res){
+app.get('/',
+	passport.authenticate('local',
+	{failureRedirect: '/welcome' }), 
+	function(req, res) {
+  	res.redirect('query/index');
+});
+
+app.get('/welcome', function (req, res){
   res.render('welcome/index');
 });
 
-app.get('/dashboard', function(req, res) {
-  res.render('query/index');
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/welcome');
 });
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+      return next();
+  res.redirect('/welcome');
+}
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-var express          = require('express');
-var app              = express();
-var handlebars       = require('express-handlebars');
-var apiRouter        = require('./routes/api-router');
-var appRouter        = require('./routes/app-router');
+var express       = require('express');
+var app           = express();
+var bodyParser    = require('body-parser');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+var passport      = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var flash         = require('connect-flash');
+var handlebars    = require('express-handlebars');
+var bcrypt        = require('bcrypt');
+var apiRouter     = require('./routes/api-router');
+var appRouter     = require('./routes/app-router');
 
 // Handlebars
 var viewDirectories = [
@@ -17,17 +24,34 @@ var hbs = handlebars.create({
     partialsDir: viewDirectories
   });
 
-// Config
+// Express Config
 var root = __dirname + '/public';
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 app.engine('hbs', hbs.engine);
+app.use(cookieParser());
+app.use(bodyParser());
 app.use(express.static(root));
 
+app.use(session({ secret: 'dudemanjones' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
 
 // Routes
 app.get('/', appRouter);
-app.get('/dashboard', appRouter);
+app.get('/welcome', appRouter);
 app.post('/api/search', apiRouter);
 
 app.listen(8000, function(){
