3fdff285fadc03577681ea9366cfdb7224ff6cb5
anniebae <bae.annie@gmail.com>
2015-05-18 22:58:42 -0400

    section tab showing on click, hiding unclicked sections

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,14 +7,20 @@ var welcome = new Welcome();
 $(function(){
   $('.food-tab').on('click', function(){
     console.log('food-tab clicked');
+    $('#drinks').hide();
+    $('#attractions').hide();
     $('#food').show();
   });
   $('.drinks-tab').on('click', function(){
     console.log('drinks-tab clicked');
+    $('#food').hide();
+    $('#attractions').hide();
     $('#drinks').show();
   });
   $('.attractions-tab').on('click', function(){
     console.log('attractions-tab clicked');
+    $('#food').hide();
+    $('#drinks').hide();
     $('#attractions').show();
   });
 });
