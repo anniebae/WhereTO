cbb29858ebb2b907d29b9db91c6314ede17deeac
anniebae <bae.annie@gmail.com>
2015-05-18 22:54:15 -0400

    sections tab div given display hidden. js making div show on click

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,3 +3,18 @@ var ref = new Firebase(URL);
 var usersRef = ref.child('users');
 
 var welcome = new Welcome();
+
+$(function(){
+  $('.food-tab').on('click', function(){
+    console.log('food-tab clicked');
+    $('#food').show();
+  });
+  $('.drinks-tab').on('click', function(){
+    console.log('drinks-tab clicked');
+    $('#drinks').show();
+  });
+  $('.attractions-tab').on('click', function(){
+    console.log('attractions-tab clicked');
+    $('#attractions').show();
+  });
+});
