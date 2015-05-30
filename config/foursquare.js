a5767846826a454388164b2fc16cab61d10989e1
Evan Turner <evturn@gmail.com>
2015-05-30 18:47:09 -0400

    querying the foursquare api endpoint

--- config/foursquare.js
+++ config/foursquare.js
@@ -1,3 +1,3 @@
-var endpoint = 'https://api.foursquare.com/v2/venues/search?client_id=' + process.env.FOURSQUARE_CLIENT_ID + '&client_secret='+ process.env.FOURSQUARE_CLIENT_SECRET + '&v=20130815&ll=40.7,-74&query=donuts';
+var endpoint = 'https://api.foursquare.com/v2/venues/explore?client_id=' + process.env.FOURSQUARE_CLIENT_ID + '&client_secret='+ process.env.FOURSQUARE_CLIENT_SECRET + '&v=20130815&ll=40.7,-74&query=';
 
 module.exports = endpoint;
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,16 @@
 var express    = require('express');
 var request    = require('request');
+var bodyParser = require('body-parser');
+var urlencoded = bodyParser.urlencoded({extended: false});
 var foursquare = require('../config/foursquare');    
 var api = express.Router();
 
-api.get('/search', function(req, res) {
-  request(foursquare, function (error, response, body) {
+api.post('/search', urlencoded, function(req, res) {
+  var query = foursquare + req.body.location;
+  request(query, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log(body);
+      res.json(body);
     }
   });
 });
