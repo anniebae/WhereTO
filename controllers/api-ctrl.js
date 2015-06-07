2a90829ec52fd6d557cf19a42f08c1254579a95f
Evan Turner <evturn@gmail.com>
2015-06-07 11:43:32 -0400

    created api-ctrl for ajax post requests to server and proxy get requests to api

---
+++ controllers/api-ctrl.js
@@ -0,0 +1,14 @@
+var request    = require('request');
+var yelp = require('../config/yelp');
+
+exports.getYelpData = function(req, res) {
+  if (!req.body) return res.sendStatus(400)
+  var request = req.body;
+  var location = request.location.split(' ').join('+');
+  var term = request.term;
+  yelp.search({term: term, location: location}, function(error, data) {
+    console.log(error);
+    console.log(data);
+    res.json(data);
+  });
+}
